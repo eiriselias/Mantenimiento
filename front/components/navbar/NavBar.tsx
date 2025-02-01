550b669376189a4371cae4414e019ec21a1291fd
eiris elias <eiriselias@gmail.com>
2025-02-01 13:24:18 -0500

    creacion la pagina 404

--- front/components/navbar/NavBar.tsx
+++ front/components/navbar/NavBar.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import Link from 'next/link'
 
 const NavBar = () => {
   return (
     <nav className='flex justify-between mx-16 p-4 font-bold'>
-       <h1 className='hover:scale-105 cursor-pointer'>Mantenimiento</h1>
+       <h1 className='hover:scale-105'><Link href="/">Mantenimiento</Link></h1>
        <ul className='flex gap-8'>
-        <li className='hover:scale-105 cursor-pointer'>Motores</li>
-        <li className='hover:scale-105 cursor-pointer'>Aceites</li>
-        <li className='hover:scale-105 cursor-pointer'>Mantenimientos</li>
-        <li className='hover:scale-105 cursor-pointer'>Inspecciones</li>
-        <li className='hover:scale-105 cursor-pointer'>Reparaciones</li>
-        <li className='hover:scale-105 cursor-pointer'>Reportes</li>
+        <li className='hover:scale-105'><Link href="#">Motores</Link></li>
+        <li className='hover:scale-105'><Link href="#">Aceites</Link></li>
+        <li className='hover:scale-105'><Link href="#">Mantenimientos</Link></li>
+        <li className='hover:scale-105'><Link href="#">Inspecciones</Link></li>
+        <li className='hover:scale-105'><Link href="#">Reparaciones</Link></li>
+        <li className='hover:scale-105'><Link href="#">Reportes</Link></li>
        </ul>
-       <span className='hover:scale-105 cursor-pointer'>Iniciar Sesion</span>
+       <span className='hover:scale-105'><Link href="#">Iniciar Sesion</Link></span>
     </nav>
   )
 }
